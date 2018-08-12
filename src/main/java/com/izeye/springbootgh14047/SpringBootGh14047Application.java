c6c1bb45c09d74b65a927a21877ee7b79ddc811c
Johnny Lim <izeye@naver.com>
2018-08-12 22:20:45 +0900

    Create a project to try to reproduce https://github.com/spring-projects/spring-boot/issues/14047

---
+++ src/main/java/com/izeye/springbootgh14047/SpringBootGh14047Application.java
@@ -0,0 +1,12 @@
+package com.izeye.springbootgh14047;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGh14047Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGh14047Application.class, args);
+	}
+}
---
+++ src/test/java/com/izeye/springbootgh14047/SpringBootGh14047ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.izeye.springbootgh14047;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootGh14047ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
